41b95d6b81a5d56f91fa3aa447196540db6d8752
Raphael Assenat <raph@raphnet.net>
2019-03-17 16:34:29 -0400

    Write in BG1 RAM buffer instead of touching VRAM

--- text.asm
+++ text.asm
@@ -1,9 +1,9 @@
 .include "header.inc"
-.include "snesregs.inc"
+;.include "snesregs.inc"
 .include "misc_macros.inc"
 .include "zeropage.inc"
 
-.define TEXT_SCREEN_WORD_PITCH	32
+.define TEXT_SCREEN_BYTE_PITCH	64
 
 ; define characters used for drawing boxes
 .define FIRST_BOX_CHAR	$80
@@ -17,9 +17,10 @@
 .define BOX_CHAR_VL FIRST_BOX_CHAR+7 ; vertical bar, left
 .define BOX_CHAR_FILL FIRST_BOX_CHAR+8 ; Filler
 
-; word addresses for bgmap in VRAM
-.define TEXT_SCREEN_VRAM_START	$1800
-.define TEXT_SCREEN_VRAM_MAX	TEXT_SCREEN_VRAM_START+32*32
+; word addresses for bgmap in bank 7F
+.define TEXT_SCREEN_RAM_START	$0
+.define TEXT_SCREEN_RAM_MAX	TEXT_SCREEN_RAM_START+32*32
+.define TEXT_SCREEN_RAM_BANK	$7F
 
 .bank 0 slot 1
 
@@ -28,7 +29,7 @@
 	text_cursor_y: dw
 
 
-	text_vram_ptr: dw ; word address for current cursor x,y
+;	text_vram_ptr: dw ; word address for current cursor x,y
 
 	; arguments for drawing text boxes
 	text_box_x: dw
@@ -88,29 +89,18 @@ text_putbcd:
 	; A: character
 	; Y: Y coordinate
 	;
-	; Warning: This access PPU registers directly. Use from vblank interrupt,
-	; force vblank, or avoid conflicts using other means..
-	;
 text_putchar:
 	pushall
 
 	XY16
-	A16
+	A8
 
-	; Set pointer in VRAM
-	ldx text_vram_ptr
-	stx VMADDL
+	sta [dp_text_ptr]
+	inc dp_text_ptr
+	inc dp_text_ptr
 
-	; Write the character
-	and #$ff
-	sta VMDATAL
-
-	inx			; Increase vram pointer
-	cpx #TEXT_SCREEN_VRAM_MAX
-	bne @done
-	ldx #TEXT_SCREEN_VRAM_START
-@done:
-	stx text_vram_ptr
+	lda #1
+	sta bg1_need_resync
 
 	popall
 	rts
@@ -122,32 +112,29 @@ text_putchar:
 	;
 	; Y: 16-bit pointer to zero-terminated string
 	;
-	; Warning: This access PPU registers directly. Use from vblank interrupt,
-	; force vblank, or avoid conflicts using other means..
-	;
 text_print:
 	pushall
 
 	XY16
-	A16
+	A8
 
-	; Set pointer in VRAM
-	ldx text_vram_ptr
-	stx VMADDL
+	; Set pointer in RAM
+	ldx dp_text_ptr
 
 @next_char:
 	lda 0, Y	; Get new character
-	and #$ff	; drop the upper byte we don't want
 	beq @done	; 0 means end of string
-	sta VMDATAL ; Write to BG
+	sta [dp_text_ptr]
 	iny			; Advance to next ASCII character
-	inx			; Increase vram pointer
-	cpx #TEXT_SCREEN_VRAM_MAX
-	bne @next_char
-	ldx #TEXT_SCREEN_VRAM_START		; Got back to home
+	inc dp_text_ptr		; Increase vram pointer
+	inc dp_text_ptr		; Increase vram pointer
 	bra @next_char
+
 @done:
-	stx text_vram_ptr
+	stx dp_text_ptr
+
+	lda #1
+	sta bg1_need_resync
 
 	popall
 	rts
@@ -156,43 +143,45 @@ text_print:
 	;
 	; Erase a box
 	;
-	; Warning: This access PPU registers directly. Use from vblank interrupt,
-	; force vblank, or avoid conflicts using other means..
-	;
 text_clearBox:
 	pushall
 
 	A16
 	XY16
 
-	; This sets text_vram_ptr to the upper-left corner of the box
+	; This sets dp_text_ptr to the upper-left corner of the box
 	ldx text_box_x
 	ldy text_box_y
 	jsr text_gotoxy
 
-	; Set pointer in VRAM
-	ldx text_vram_ptr
-	stx VMADDL
+	; save line origin
+	lda dp_text_ptr
+	sta dp_text_tmp
+
 
 	ldy text_box_h  ; Load height in X
 @lp_y:
 	ldx text_box_w	; Load width in X
 @lp_x:
-	stz VMDATAL
+	lda #0
+	sta [dp_text_ptr]
+	inc dp_text_ptr
+	inc dp_text_ptr
 	dex
 	bne @lp_x
 
-	lda text_vram_ptr
+	lda dp_text_tmp
 	clc
-	adc #TEXT_SCREEN_WORD_PITCH
-	sta text_vram_ptr
-	sta VMADDL
-
-	wai
+	adc #TEXT_SCREEN_BYTE_PITCH
+	sta dp_text_tmp
+	sta dp_text_ptr
 
 	dey
 	bne @lp_y
 
+	A8
+	lda #1
+	sta bg1_need_resync
 
 	popall
 
@@ -202,104 +191,117 @@ text_clearBox:
 	;
 	; Draw a box
 	;
-	; Warning: This access PPU registers directly. Use from vblank interrupt,
-	; force vblank, or avoid conflicts using other means..
-	;
 text_drawBox:
 	pushall
 
 	A16
 	XY16
 
-	; This sets text_vram_ptr to the upper-left corner of the box
+	; This sets dp_text_ptr to the upper-left corner of the box
 	ldx text_box_x
 	ldy text_box_y
 	jsr text_gotoxy
 
-	; Set pointer in VRAM
-	ldx text_vram_ptr
-	stx VMADDL
-
-	ldy text_box_h  ; Load height in X
+	; Save start of line ptr
+	ldx dp_text_ptr
+	stx dp_text_tmp
 
 	;;; Top line of the box ;;;
+	ldx dp_text_ptr
 
-	ldx text_box_w	; Load width in X
+;	ldx text_box_w	; Load width in X
 	; Upper left corner
 	lda #BOX_CHAR_UL
-	sta VMDATAL
-	dex
+	sta [dp_text_ptr]
+	inc dp_text_ptr
+	inc dp_text_ptr
+
 	; Horizontal bar, top
+	ldy text_box_w  ; Load height in W
+	dey
+	dey
 	lda #BOX_CHAR_HT
 @lp_horiz_top:
-	sta VMDATAL
-	dex
-	cpx #1
+	sta [dp_text_ptr]
+	inc dp_text_ptr
+	inc dp_text_ptr
+	dey
 	bne @lp_horiz_top
+
 	; Upper right corner
 	lda #BOX_CHAR_UR
-	sta VMDATAL
-	dey
+	sta [dp_text_ptr]
+	inc dp_text_ptr
+	inc dp_text_ptr
+
 
 	;;; Middle lines of the box ;;;
 
-	; Advance vram pointer to next line
+	ldy text_box_h  ; Load height in Y
+	dey 	; subtract top and bottom lines
+	dey
+
 @inner_lines_lp:
-wai
-	lda text_vram_ptr
+	lda dp_text_tmp	; Get start of previous line
 	clc
-	adc #TEXT_SCREEN_WORD_PITCH
-	sta text_vram_ptr
-	sta VMADDL
+	adc #TEXT_SCREEN_BYTE_PITCH	; advance to next line
+	sta dp_text_tmp ; save it for next tome
+	sta dp_text_ptr	; point to it now
 
 
 	ldx text_box_w	; Load width in X
 
 	; Vertical bar left
 	lda #BOX_CHAR_VL
-	sta VMDATAL
+	sta [dp_text_ptr]
+	inc dp_text_ptr
+	inc dp_text_ptr
 	dex
 	; Background filler
 	lda #BOX_CHAR_FILL
 @lp_horiz_fill:
-	sta VMDATAL
+	sta [dp_text_ptr]
+	inc dp_text_ptr
+	inc dp_text_ptr
 	dex
 	cpx #1
 	bne @lp_horiz_fill
 	; Vertical bar right
 	lda #BOX_CHAR_VR
-	sta VMDATAL
+	sta [dp_text_ptr]
+	inc dp_text_ptr
+	inc dp_text_ptr
 
 	dey
-	cpy #1
 	bne @inner_lines_lp
 
 	;;; Bottom line of the box ;;;
-
-	lda text_vram_ptr
+	lda dp_text_tmp	; Get start of previous line
 	clc
-	adc #TEXT_SCREEN_WORD_PITCH
-	sta text_vram_ptr
-	sta VMADDL
-
+	adc #TEXT_SCREEN_BYTE_PITCH	; advance to next line
+	sta dp_text_tmp ; save it for next tome
+	sta dp_text_ptr	; point to it now
 
 	ldx text_box_w	; Load width in X
 	; Lower left corner
 	lda #BOX_CHAR_LL
-	sta VMDATAL
+	sta [dp_text_ptr]
+	inc dp_text_ptr
+	inc dp_text_ptr
 	dex
 	; Horizontal bar, bottom
 	lda #BOX_CHAR_HB
 @lp_horiz_bottom:
-	sta VMDATAL
+
+	sta [dp_text_ptr]
+	inc dp_text_ptr
+	inc dp_text_ptr
 	dex
 	cpx #1
 	bne @lp_horiz_bottom
 	; Lower right corner
 	lda #BOX_CHAR_LR
-	sta VMDATAL
-	dey
-
+	sta [dp_text_ptr]
 
 
 	popall
@@ -309,6 +311,10 @@ _text_xy_to_ptr:
 	pushall
 
 	A8
+	; set bank
+	lda #TEXT_SCREEN_RAM_BANK
+	sta dp_text_ptr + 2
+
 	lda text_cursor_y
 	A16
 	XY16
@@ -318,10 +324,13 @@ _text_xy_to_ptr:
 	asl ; *8
 	asl ; *16
 	asl ; *32
+	asl ; *32
 
 	adc text_cursor_x
-	adc #TEXT_SCREEN_VRAM_START
-	sta text_vram_ptr
+	adc text_cursor_x
+	adc #TEXT_SCREEN_RAM_START
+	sta dp_text_ptr
+
 
 	popall
 	rts
